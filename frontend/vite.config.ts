40ace0fe5b7553c2d0c5f0a453e2c80593452915
“Djisu” <pfleischer2002@yahoo.co.uk>
2024-09-23 22:54:48 +0000

    Correcting vite config for node-production

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,4 +6,7 @@ export default defineConfig({
   server: {
     open: true, // Automatically open the browser
   },
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+  },
 });
